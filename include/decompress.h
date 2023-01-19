0f3a97e22200a383f2d208aa7f528615fcc3c2c8
Mariel <96346749+yel09@users.noreply.github.com>
2023-01-20 01:19:44 +0800

    Add files via upload

---
+++ include/decompress.h
@@ -0,0 +1,16 @@
+#ifndef DECOMPRESS_H
+#define DECOMPRESS_H
+
+
+class decompress
+{
+    public:
+        decompress();
+        virtual ~decompress();
+
+    protected:
+
+    private:
+};
+
+#endif // DECOMPRESS_H
